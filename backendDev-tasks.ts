9b976c48c0c19d42e43f3bc58157dbc104cbcc57
Zhuoxuan C <zhuoxuan.chen00@gmail.com>
2026-08-27 13:16:04 +0200

    skill 1 task 2

--- backendDev-tasks.ts
+++ backendDev-tasks.ts
@@ -1,9 +1,19 @@
-let studentName:string = "Hsuan";
-let studentAge:number = 26;
-let isEnrolled:boolean = true;
+// let studentName:string = "Hsuan";
+// let studentAge:number = 26;
+// let isEnrolled:boolean = true;
 
-const describeStudent = (name:string, age:number) => {
-    return `${name} is ${age} years old.`
+// const describeStudent = (name:string, age:number) => {
+//     return `${name} is ${age} years old.`
+// }
+
+// console.log (describeStudent(studentName, studentAge));
+
+
+let name1:string = "Hsuan";
+let formal:boolean= false;
+
+const formatGreeting = (name:string, formal?:boolean) =>{
+    return formal ? `Good day,${name}.` : `Hi ${name}!`;
 }
 
-console.log (describeStudent(studentName, studentAge));
+console.log (formatGreeting(name1, formal))
