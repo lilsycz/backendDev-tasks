7dd12cd97ac4a2f93c20c86ac8cd802f1e1729d4
Zhuoxuan C <zhuoxuan.chen00@gmail.com>
2026-08-28 00:21:12 +0200

    skill 3.1

--- backendDev-tasks.ts
+++ backendDev-tasks.ts
@@ -20,34 +20,50 @@
 
 // console.log (formatGreeting(name1, formal));
 
-// //-- skill 2.1--//
 
+// //-- skill 2.1--//
 // let ages:number[]=[8,21.56,34,78];
 // const agesInFiveYears:number[] = ages.map((n) => n+5);
 // console.log(agesInFiveYears);
 
-// //--skill 2.2--//
 
+// //--skill 2.2--//
 // let names:string[]=["Max", "Jade", "Josh", "Jake", "Hsuan", "Layla"];
 // const shortNames:string[] = names.filter((n)=> n.length <= 4);
 // console.log (shortNames);
 
-//--skill 2.3--//
 
-let scores:number[]=[32,59,78,2,100,66,97,43];
-const passingScores:number[] = scores.filter (n=>(n>=50));
-const grade:string[] = passingScores.map ((score) => {
-    if (score >= 90)
-        return "A";
-    else if (score>=80)
-        return "B";
-    else if (score>=70)
-        return "C";
-    else if (score>=60)
-        return "D";
-    else
-        return "E";
-});
-const failedStudents:number[] = scores.filter (n=>(n<50));
-console.log (failedStudents.length);
-console.log (grade);
+// //--skill 2.3--//
+// let scores:number[]=[32,59,78,2,100,66,97,43];
+// const passingScores:number[] = scores.filter (n=>(n>=50));
+// const grade:string[] = passingScores.map ((score) => {
+//     if (score >= 90)
+//         return "A";
+//     else if (score>=80)
+//         return "B";
+//     else if (score>=70)
+//         return "C";
+//     else if (score>=60)
+//         return "D";
+//     else
+//         return "E";
+// });
+// const failedStudents:number[] = scores.filter (n=>(n<50));
+// console.log (failedStudents.length);
+// console.log (grade);
+
+
+//--skill 3.1--//
+interface Book {
+    title:string;
+    author:string;
+    pages:number;
+}
+
+const myBook: Book = {
+    title: "A Brief History of Human",
+    author: "Yuval Noah Harari",
+    pages:55,
+};
+
+console.log (`I read "${myBook.title}".`);
