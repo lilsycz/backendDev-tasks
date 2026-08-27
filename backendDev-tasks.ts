62b0d53f4e2d9f70972013c0b307221089ab36b8
Zhuoxuan C <zhuoxuan.chen00@gmail.com>
2026-08-28 00:09:58 +0200

    skill 2.3

--- backendDev-tasks.ts
+++ backendDev-tasks.ts
@@ -26,8 +26,28 @@
 // const agesInFiveYears:number[] = ages.map((n) => n+5);
 // console.log(agesInFiveYears);
 
-//--skill 2.2--//
+// //--skill 2.2--//
 
-let names:string[]=["Max", "Jade", "Josh", "Jake", "Hsuan", "Layla"];
-const shortNames:string[] = names.filter((n)=> n.length <= 4);
-console.log (shortNames);
+// let names:string[]=["Max", "Jade", "Josh", "Jake", "Hsuan", "Layla"];
+// const shortNames:string[] = names.filter((n)=> n.length <= 4);
+// console.log (shortNames);
+
+//--skill 2.3--//
+
+let scores:number[]=[32,59,78,2,100,66,97,43];
+const passingScores:number[] = scores.filter (n=>(n>=50));
+const grade:string[] = passingScores.map ((score) => {
+    if (score >= 90)
+        return "A";
+    else if (score>=80)
+        return "B";
+    else if (score>=70)
+        return "C";
+    else if (score>=60)
+        return "D";
+    else
+        return "E";
+});
+const failedStudents:number[] = scores.filter (n=>(n<50));
+console.log (failedStudents.length);
+console.log (grade);
