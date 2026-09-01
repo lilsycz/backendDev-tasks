e39adc13aff00e53b13081a6eacbdaddfd99c84b
Zhuoxuan C <zhuoxuan.chen00@gmail.com>
2026-09-01 22:34:09 +0200

    skill-2.2

--- week3/skill-2.ts
+++ week3/skill-2.ts
@@ -1,11 +1,35 @@
-interface Book {
-    title: string,
-    pages: number,
-}
+// interface Book {
+//     title: string,
+//     pages: number,
+// }
 
 
-const describeBook = (book: Book) => {
-    console.log (`The book ${book.title} has ${book.pages} pages.`)
-}
+// const describeBook = (book: Book) => {
+//     console.log (`The book ${book.title} has ${book.pages} pages.`)
+// }
 
-describeBook({title:"Dune", pages:412});
+// describeBook({title:"Dune", pages:412});
+
+
+interface Teacher {
+    name: string,
+    subject: string,
+};
+
+interface Employee {
+    id: number,
+    email: string
+};
+
+type SchoolTeacher = Teacher & Employee;
+
+const printTeacherInfo = (schoolTeacher: SchoolTeacher) => {
+    console.log (`The ${schoolTeacher.subject} teacher is ${schoolTeacher.name}, her id is ${schoolTeacher.id} and her email is ${schoolTeacher.email}.`)
+};
+
+printTeacherInfo ({
+    name:"H", 
+    subject:"math", 
+    id:333, 
+    email:"xxx@lu.se"
+})
